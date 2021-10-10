45d7ebbbe348ee63bdfd2b8afcab3a49b0cb091b
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2021-10-11 02:19:22 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (30.51%) - LeetHub

--- merge-sorted-array/merge-sorted-array.cpp
+++ merge-sorted-array/merge-sorted-array.cpp
@@ -18,10 +18,11 @@ public:
             }
         }
         while(j>=0){
-            nums1[k] = nums2[j];
+            nums1[k]= nums2[j];
             k--;
             j--;
         }
+        
         
     }
 };
