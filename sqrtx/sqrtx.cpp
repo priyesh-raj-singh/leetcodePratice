8f410ffe5f20d3792daeeeaaec35c01eb11f7b2a
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2021-11-03 00:29:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.37%) - LeetHub

--- sqrtx/sqrtx.cpp
+++ sqrtx/sqrtx.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
-
-   x= floor(sqrt(x));
-   return x;
-
+        int s = 0 , e = 1e8;
+        int ans = 0 ; 
+        while(s<=e){
+            long long int m = s+(e-s)/2;
+            if(m*m<=x){
+                ans = m ;
+                s = m+1;
+            }
+            else e = m-1;
+        }
+        return ans;
         
     }
 };
