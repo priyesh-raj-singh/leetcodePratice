453bc3f199e24e633720ec07e1026b73d39648e9
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-01-16 12:18:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
+        int p = 0 ; 
+        int i = 1; 
+        int count = 2;
         if(n<=1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        while(count <=n){
+            int temp = i; 
+            i = i+p;
+            p = temp;
+            count++;
+        }
+        return i;
+        
         
     }
 };
