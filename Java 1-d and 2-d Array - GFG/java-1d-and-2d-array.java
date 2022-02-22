aad71893f8c13cdb3cc67e2171ecd9eefc2e9cfc
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-02-22 11:52:35 +0530

    Added solution - LeetHub

---
+++ Java 1-d and 2-d Array - GFG/java-1d-and-2d-array.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+// Java program to demonstrate working of Scanner in Java
+import java.util.*;
+import java.io.*;
+class Abc
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int[] a[],b;
+            a= new int[n][n];
+            b= new int[n];
+            int sum=0;
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    int p=sc.nextInt();
+                    a[i][j]=p;
+                }
+            }
+            int Max=0;
+            for(int i=0;i<n;i++)
+            {
+                int p=sc.nextInt();
+                b[i]=p;
+            }
+            Complete obj = new Complete();
+            ArrayList<Integer> ans;
+            ans = obj.array(a, b, n);
+            for(int i: ans)
+                System.out.print(i + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Complete
+{
+    public static ArrayList<Integer> array(int a[][], int b[], int n)
+    {
+        // Complete the function
+        ArrayList<Integer> res = new ArrayList<Integer>();
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+        sum += a[i][i];
+    Arrays.sort(b);
+    res.add(sum);
+    res.add(b[n-1]);
+    return res;
+    }
+}
