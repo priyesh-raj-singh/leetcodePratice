6c607490b12a322e963ae9d5bd049bee87452dc7
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-02-22 22:53:52 +0530

    Time: 23 ms (79.79%), Space: 19.3 MB (13.79%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int index = 0;
+        for(int i = 0 ; i<nums.size() ; i++){
+            if(nums[i]!=0){
+                nums[index] = nums[i];
+                index++;
+            }
+            
+        }
+        for( ; index<nums.size() ; index++){
+            nums[index] = 0;
+        }
+        
+        
+        
+    }
+};
