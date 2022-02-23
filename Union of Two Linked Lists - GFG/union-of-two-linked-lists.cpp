cdf9846651f1afd1580985221ca5db070aed5c83
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-02-23 10:10:53 +0530

    Added solution - LeetHub

---
+++ Union of Two Linked Lists - GFG/union-of-two-linked-lists.cpp
@@ -0,0 +1,110 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+struct Node* makeUnion(struct Node* head1, struct Node* head2);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        
+        printList(makeUnion(first,second));
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/*
+// structure of the node is as follows
+
+struct node
+{
+	int data;
+	struct node* next;
+	
+	node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+*/
+
+struct Node* makeUnion(struct Node* head1, struct Node* head2)
+{
+    // code here
+    set<int> s;
+    struct Node * p = head1 , *q = head2;
+    while(p){
+        s.insert(p->data);
+        p = p->next;
+    }
+    while(q){
+        s.insert(q->data);
+        q = q->next;
+    }
+    struct Node *head = nullptr , *r = nullptr;
+    for(set<int>::iterator it = s.begin(); it != s.end(); it++){
+        if(!head){
+            head = new struct Node(*it);
+            r = head;
+        }
+        else{
+            r->next = new struct Node(*it);
+            r = r->next;
+        }
+    }
+    return head;
+}
