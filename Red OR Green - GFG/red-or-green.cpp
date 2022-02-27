36d997af2a95e8a8b112813b97535bcdf154949b
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-02-27 11:23:55 +0530

    Added solution - LeetHub

---
+++ Red OR Green - GFG/red-or-green.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+       int RedOrGreen(int N,string S) {
+           //code here
+           unordered_map<char , int> mp;
+           for(int i = 0 ; i<S.size() ; i++){
+               mp[S[i]]++;
+           }
+           return min(mp['G'] , mp['R']);
+                    
+           
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.RedOrGreen(N,S) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
