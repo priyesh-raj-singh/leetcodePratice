ac869af93829e0e0999232447d7c892499431619
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-03-13 12:28:43 +0530

    Time: 3 ms (35.76%), Space: 6.4 MB (10.36%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<char> st;
+        bool ans = true ;
+        for(int i = 0 ; i<n ; i++){
+            if(s[i] =='{' || s[i] == '(' || s[i]=='[')
+            {
+                st.push(s[i]);
+            }
+            else if(s[i] == ')'){
+                if(!st.empty() && st.top() == '('){
+                    st.pop();
+                }
+                else{
+                    ans = false ;
+                    break ;
+                }
+            }
+            else if(s[i] == ']'){
+                if(!st.empty() && st.top() == '['){
+                    st.pop();
+                }
+                else{
+                    ans = false ;
+                    break ;
+                }
+            }
+            else if(s[i] == '}'){
+                if(!st.empty() && st.top() == '{'){
+                    st.pop();
+                }
+                else{
+                    ans = false ;
+                    break ;
+                }
+            }
+            if(st.empty()){
+                ans = true ;
+            }
+         else {
+             ans = false ;
+         }
+            
+        }
+        return ans ;
+        
+    }
+};
