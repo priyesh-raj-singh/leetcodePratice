76f0fbd5eef2abb4297c1416b8de592ff5922c37
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-03-16 11:09:01 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (84.36%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int sumsq(int n){
-        int sum = 0;
+    int sumsq(int n ){
+        int sum = 0 ; 
         while(n>0){
             sum+=(n%10)*(n%10);
             n = n/10;
@@ -12,7 +12,7 @@ public:
         while(n>6){
             n = sumsq(n);
         }
-        if(n!=1) 
+        if(n!=1)
             return false;
         
         return true;
