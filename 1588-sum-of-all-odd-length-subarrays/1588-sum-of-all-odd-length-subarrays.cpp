382f8e0ec2487c8e6685da4877edf31722da00a5
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-03-18 13:52:52 +0530

    Time: 7 ms (40.66%), Space: 8.4 MB (39.84%) - LeetHub

---
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> preSum(n+2);
+        int sum = 0 ; 
+        // preSum[0] = 0;
+        
+        
+        for(int i=0 ; i<n ; i++){
+            preSum[i+1] = arr[i]+preSum[i];
+        }
+        for(int i = 0 ;i<n ; i++){
+            for(int j = i ;j <n ; j+=2){
+                if(i==j)
+                    sum+=arr[i];
+                
+                else
+                    sum+=preSum[j+1]-preSum[i];
+            }
+        }
+        return sum;
+        
+    }
+};
