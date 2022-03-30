cfb88281c1819656ac21d3400777c6bf84025463
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-03-30 12:41:38 +0530

    Added solution - LeetHub

---
+++ Counts Zeros Xor Pairs - GFG/counts-zeros-xor-pairs.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+// C++ program to find number 
+// of pairs in an array such
+// that their XOR is 0
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to calculate the
+// count
+long long int calculate(int a[], int n);
+
+// Driver Code
+int main()
+{
+
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	  int n;
+	  cin>>n;
+	  int arr[n+1];
+	  for( int i=0;i<n;i++)
+	        cin>>arr[i];
+	    cout << calculate(arr, n)<<endl;
+        
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+long long int calculate(int a[], int n)
+{
+    // Complete the function
+    unordered_map<int, long long > mp;
+    long long ans = 0 ;
+    for(int i = 0 ; i<n ; i++){
+        ans+=mp[a[i]];
+        mp[a[i]]++;
+    }
+    return ans;
+}
